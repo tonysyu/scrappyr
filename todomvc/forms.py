9be8aac69ec965abe83790ddb4f5eff19d734c55
Clinton Dreisbach <clinton@dreisbach.us>
2015-03-04 11:19:57 -0500

    Create API for TodoMVC Angular app

--- todomvc/forms.py
+++ todomvc/forms.py
@@ -1,3 +1,17 @@
 from flask_wtf import Form
 
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+
 """Add your forms here."""
+
+class APIForm(Form):
+    def __init__(self, *args, **kwargs):
+        default_kwargs = {"formdata": None, "csrf_enabled": False}
+        default_kwargs.update(kwargs)
+        super().__init__(*args, **default_kwargs)
+
+class TodoForm(APIForm):
+    title = StringField('Title', validators=[DataRequired()])
+    completed = BooleanField('Completed', default=False)
--- todomvc/views.py
+++ todomvc/views.py
@@ -3,21 +3,67 @@
 We have started you with an initial blueprint. Add more as needed.
 """
 
-from flask import Blueprint, flash
+from flask import Blueprint, jsonify, request
+from .models import Todo
+from .forms import TodoForm
+from .extensions import db
 
-
-todomvc = Blueprint("todomvc", __name__)
-
+todomvc = Blueprint("todomvc", __name__, static_folder="./static")
 
 @todomvc.route("/")
 def index():
-    return "Hello, world!"
+    return todomvc.send_static_file("index.html")
+
+@todomvc.route("/api")
+def api():
+    return ""
+
+@todomvc.route("/api/todos", methods=["GET"])
+def todos():
+    todos = [todo.to_dict() for todo in Todo.query.all()]
+    return jsonify(todos=todos)
+
+@todomvc.route("/api/todos", methods=["POST"])
+def add_todo():
+    todo_data = request.get_json()
+    form = TodoForm(data=todo_data)
+    if form.validate():
+        todo = Todo(**form.data)
+        db.session.add(todo)
+        db.session.commit()
+        return jsonify(todo.to_dict())
+    else:
+        resp = jsonify(form.errors)
+        resp.status_code = 400
+        return resp
+
+@todomvc.route("/api/todos/<int:id>", methods=["PUT"])
+def update_todo(id):
+    todo = Todo.query.get_or_404(id)
+    todo_data = request.get_json()
+    form = TodoForm(data=todo_data)
+    if form.validate():
+        form.populate_obj(todo)
+        db.session.commit()
+        return jsonify(todo.to_dict())
+    else:
+        resp = jsonify(form.errors)
+        resp.status_code = 400
+        return resp
+
+@todomvc.route("/api/todos/<int:id>", methods=["DELETE"])
+def delete_todo(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify({"deleted": "true"})
 
 
-def flash_errors(form, category="warning"):
-    '''Flash all errors for a form.'''
-    for field, errors in form.errors.items():
-        for error in errors:
-            flash("{0} - {1}"
-                    .format(getattr(form, field).label.text, error), category)
+
+
+
+
+
+
+
 
