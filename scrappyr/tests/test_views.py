67c3f64acb1765f4b1baa80c3f622bfc72588382
Tony S Yu <tsyu80@gmail.com>
2015-09-19 15:19:30 -0500

    Fix tag instantiation in form

--- scrappyr/tests/test_views.py
+++ scrappyr/tests/test_views.py
@@ -8,6 +8,7 @@ from ..models import Scrap
 
 TAG = 'dummy-tag-for-testing'
 TITLE = 'Dummy title for testing'
+TAG_DATA_LIST = [{'text': TAG}]
 
 
 def add_to_db(obj):
@@ -19,7 +20,7 @@ def count_scraps():
     return len(db.session.query(Scrap).all())
 
 
-class TestView(TestCase):
+class TestHarness(TestCase):
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
@@ -34,6 +35,9 @@ class TestView(TestCase):
     def teardown(self):
         db.session.remove()
         db.drop_all()
+
+
+class TestBasicView(TestHarness):
 
     def test_get_from_empty_db(self):
         response = self.client.get('/api/scraps')
@@ -61,17 +65,6 @@ class TestView(TestCase):
         assert scrap['tags'] == []
         assert TITLE in scrap['html_title']
 
-    def test_post_with_tag(self):
-        data = {'title': TITLE, 'tags': [{'text': TAG}]}
-        json_data = json.dumps(data)
-        response = self.client.post('/api/scraps', data=json_data,
-                                    content_type='application/json')
-        assert response.status_code == 200
-        scrap = response.json
-        assert scrap['title'] == TITLE
-        assert scrap['tags'] == [{'text': TAG}]
-        assert TITLE in scrap['html_title']
-
     def test_put(self):
         add_to_db(Scrap(title='Original title'))
         data = {'title': TITLE}
@@ -88,3 +81,27 @@ class TestView(TestCase):
         response = self.client.delete('/api/scraps/1')
         assert response.status_code == 200
         assert count_scraps() == 0
+
+
+class TestTags(TestHarness):
+
+    def test_post(self):
+        data = {'title': TITLE, 'tags': TAG_DATA_LIST}
+        json_data = json.dumps(data)
+        response = self.client.post('/api/scraps', data=json_data,
+                                    content_type='application/json')
+        assert response.status_code == 200
+        scrap = response.json
+        assert scrap['title'] == TITLE
+        assert scrap['tags'] == TAG_DATA_LIST
+        assert TITLE in scrap['html_title']
+
+    def test_put(self):
+        add_to_db(Scrap(title=TITLE))
+        data = {'title': TITLE, 'tags':  TAG_DATA_LIST}
+        response = self.client.put('/api/scraps/1', data=json.dumps(data),
+                                   content_type='application/json')
+        assert response.status_code == 200
+        scrap = response.json
+        assert scrap['title'] == TITLE
+        assert scrap['tags'] == TAG_DATA_LIST
