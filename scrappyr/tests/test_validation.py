6af9058540e68babdf83529de687c5df625080bf
Tony S Yu <tsyu80@gmail.com>
2015-09-21 22:00:27 -0500

    Remove WTForms dependency (replaced by Schematics in previous commit)

---
+++ scrappyr/tests/test_validation.py
@@ -0,0 +1,35 @@
+import pytest
+from schematics.exceptions import ModelConversionError
+
+from ..validation import ScrapForm
+
+
+TAG = 'dummy-tag-for-testing'
+TITLE = 'Dummy title for testing'
+
+
+def test_basic():
+    data = {'title': TITLE, 'tags': [{'text': TAG}]}
+    form = ScrapForm(data)
+    scrap = form.to_primitive()
+    assert scrap['title'] == data['title']
+    assert scrap['tags'] == data['tags']
+    assert form.validation_errors() is None
+
+
+def test_bad_title():
+    with pytest.raises(ModelConversionError):
+        ScrapForm({'title': {}})
+
+
+def test_bad_tag():
+    with pytest.raises(ModelConversionError):
+        ScrapForm({'title': TITLE, 'tags': [{'bad-key': 'tag'}]})
+
+
+def test_multiple_tags():
+    tags = [{'text': TAG}, {'text': 'Another-tag'}]
+    form = ScrapForm({'title': TITLE, 'tags': tags})
+    scrap = form.to_primitive()
+    assert scrap['tags'] == tags
+    assert form.validation_errors() is None
