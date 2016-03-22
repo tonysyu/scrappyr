b9848fa21d467f478a894ef4c4e786a279d011e5
Tony S Yu <tsyu80@gmail.com>
2016-03-21 22:51:03 -0500

    Move TagStorage to core subpackage

--- scrappyr/static/src/ng-ui/services/tagStorage.js
+++ scrappyr/static/src/ng-ui/services/tagStorage.js
@@ -1,31 +1,4 @@
-import {createMappedArray} from '../../core/utils';
-
-/**
- * Service that persists and retrieves tags from the backend API.
- */
-class TagStorage {
-    constructor($http) {
-        this._http = $http;
-        this.tags = createMappedArray();
-    }
-
-    remove(tag) {
-        var originalTags = this.tags.copy();
-        this.tags.remove(tag.id);
-
-        var success = () => this.tags;
-        var failure = () => {
-            this.tags.update(originalTags);
-            return this.tags;
-        }
-        return this._http.delete('/api/tags/' + tag.id).then(success, failure);
-    }
-
-    get() {
-        return this._http.get('/api/tags')
-            .then((response) => this.tags.update(response.data.tags));
-    }
-}
+import TagStorage from '../../core/tagStorage';
 
 function tagStorageFactory($http){
     return new TagStorage($http);
